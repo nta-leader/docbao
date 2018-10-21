2e7a12c32af89c2aec1682904b66b92f06e74f2c
nta-leader <theanh_a1k12@yahoo.com>
2018-10-21 18:00:52 +0700

    hoan thanh

--- app/Http/Controllers/Docbao/IndexController.php
+++ app/Http/Controllers/Docbao/IndexController.php
@@ -4,28 +4,57 @@ namespace App\Http\Controllers\Docbao;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Model\Admin\Danhmuc\AdModel;
 use Response;
 use DB;
 
 class IndexController extends Controller
 {
-    public function index(){
-        $arItem=DB::table('tintuc')->get()->toArray();
-        /*$arItem=[
-            "0"=>[
-                "id"=>"1",
-                "name"=>"thời sự",
-                "dmc"=>[
-                    "id"=>"2",
-                    "name"=>"trong nước"
-                ]
-            ],
-            "1"=>[
-                "id"=>"1",
-                "name"=>"thời sự",
-                "dmc"=>""
-            ],   
-        ];*/
+    public function list_dm(AdModel $cat){
+        $objCha=$cat->getList_cha();
+        $objCon=$cat->getList_con();
+        foreach($objCha as $cha){
+            $arItem[$cha->danhmuc_id]=$cha->toArray();
+            $i=0;
+            foreach($objCon as $con){
+                if($cha->danhmuc_id==$con->parent_id){
+                    $arItem[$cha->danhmuc_id]['con'][$con->danhmuc_id]=$con->toArray();
+                    $i++;
+                }
+            }
+            if($i==0){ $arItem[$cha->danhmuc_id]['con']=""; }
+        }
+        $responsecode = 200;
+        $header = array (
+                'Content-Type' => 'application/json; charset=UTF-8',
+                'charset' => 'utf-8'
+            );
+        $json=response()->json($arItem , $responsecode, $header, JSON_UNESCAPED_UNICODE);
+        return $json;
+    }
+    public function index($page){
+        $arItem=DB::table('tintuc')->where('active',1)->select('tintuc_id','tentintuc','gioithieu','hinhanh')->skip($page*20-20)->take(20)->get();
+        $responsecode = 200;
+        $header = array (
+                'Content-Type' => 'application/json; charset=UTF-8',
+                'charset' => 'utf-8'
+            );
+        $json=response()->json($arItem , $responsecode, $header, JSON_UNESCAPED_UNICODE);
+        return $json;
+    }
+    public function cat($id,$page){
+        $arId=DB::table('danhmuc')->where('danhmuc_id',$id)->orWhere('parent_id',$id)->get()->toArray();
+        $arItem=DB::table('tintuc')->where('active',1)->whereIn('danhmuc_id',$arId)->select('tintuc_id','tentintuc','gioithieu','hinhanh')->skip($page*20-20)->take(20)->get();
+        $responsecode = 200;
+        $header = array (
+                'Content-Type' => 'application/json; charset=UTF-8',
+                'charset' => 'utf-8'
+            );
+        $json=response()->json($arItem , $responsecode, $header, JSON_UNESCAPED_UNICODE);
+        return $json;
+    }
+    public function detail($id){
+        $arItem=DB::table('tintuc')->where('active',1)->where('tintuc_id',$id)->select('tintuc_id','tentintuc','chitiet','hinhanh')->first();
         $responsecode = 200;
         $header = array (
                 'Content-Type' => 'application/json; charset=UTF-8',
